589df4c4c0906653d7f169533b3240d33633e0c0
Pochang Lee <pochanglee2008@gmail.com>
2022-08-08 23:27:28 +0800

    feat: add load_command

--- command.py
+++ command.py
@@ -1,30 +1,30 @@
+from dataclasses import dataclass
 from enum import Enum
 import re
 
 
-class FoxToAICommand(Enum):
-    STATUS = '$FASTATUS'
-    MOVE = '$FAMOVE'
-    SKIP = '$FASKIP'
-    RESULT = '$FARESULT'
-    RULE = '$FARULE'
-    TIME_LEFT = '$FATIMELEFT'
-    SCORE = '$FASCORE'
+class FoxToAICommands(Enum):
+    STATUS = 'FASTATUS'
+    MOVE = 'FAMOVE'
+    SKIP = 'FASKIP'
+    RESULT = 'FARESULT'
+    RULE = 'FARULE'
+    TIME_LEFT = 'FATIMELEFT'
+    SCORE = 'FASCORE'
 
 
-class AIToFoxCommand(Enum):
-    PLAY = '$AFPLAY'
-    STATUS = '$AFSTATUS'
-    SKIP = '$AFSKIP'
-    GIVE_UP = '$AFGIVEUP'
-    SCORE = '$AFSCORE'
+class AIToFoxCommands(Enum):
+    PLAY = 'AFPLAY'
+    STATUS = 'AFSTATUS'
+    SKIP = 'AFSKIP'
+    GIVE_UP = 'AFGIVEUP'
+    SCORE = 'AFSCORE'
 
 
 def get_checksum(data: str):
     assert isinstance(data, str), 'must be str instance'
     sum = 0
     for byte in data:
-        print(byte, ord(byte))
         sum ^= ord(byte)
     return hex(sum)[2:].upper().zfill(2)
 
@@ -36,3 +36,26 @@ def verify_checksum(data: str):
     checksum1 = get_checksum(m.group(1))
     checksum2 = m.group(2)
     return checksum1 == checksum2
+
+
+@dataclass
+class Command:
+    command: str
+    content: str
+
+
+@dataclass
+class FoxToAICommand(Command):
+    command: FoxToAICommands
+    data: 'list[str]'
+
+
+def load_command(data: str):
+    assert isinstance(data, str), 'must be str instance'
+    assert verify_checksum(data), 'invalid checksum'
+    m = re.search(r'^\$(.+)\*([0-9A-F]{2})\r\n$', data)
+
+    text = m.group(1)
+    command, content = text.split(',', 1)
+
+    return FoxToAICommand(command=command, content=content, data=content.split(','))
--- test_command.py
+++ test_command.py
@@ -1,4 +1,4 @@
-from command import get_checksum, verify_checksum
+from command import FoxToAICommand, load_command, get_checksum, verify_checksum
 
 
 def test_get_checksum():
@@ -9,3 +9,12 @@ def test_get_checksum():
 def test_verify_checksum():
     assert verify_checksum('$FASTATUS,0*0F\r\n')
     assert not verify_checksum('$FASTATUS,0*0E\r\n')
+
+
+def test_load_command():
+    command = load_command('$FASTATUS,0*0F\r\n')
+
+    assert isinstance(command, FoxToAICommand)
+    assert command.command == 'FASTATUS'
+    assert command.content == '0'
+    assert command.data == ['0']
