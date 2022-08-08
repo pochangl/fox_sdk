7a669165def6c42985c3c8de8594ef948bb866ad
Pochang Lee <pochanglee2008@gmail.com>
2022-08-08 23:08:49 +0800

    feat: add verify_checksum

--- command.py
+++ command.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 
 class FoxToAICommand(Enum):
@@ -20,8 +21,18 @@ class AIToFoxCommand(Enum):
 
 
 def get_checksum(data: str):
+    assert isinstance(data, str), 'must be str instance'
     sum = 0
     for byte in data:
         print(byte, ord(byte))
         sum ^= ord(byte)
     return hex(sum)[2:].upper().zfill(2)
+
+
+def verify_checksum(data: str):
+    assert isinstance(data, str), 'must be str instance'
+    m = re.search(r'^\$(.+)\*([0-9A-F]{2})\r\n$', data)
+
+    checksum1 = get_checksum(m.group(1))
+    checksum2 = m.group(2)
+    return checksum1 == checksum2
--- test_command.py
+++ test_command.py
@@ -1,6 +1,11 @@
-from command import get_checksum
+from command import get_checksum, verify_checksum
 
 
 def test_get_checksum():
     cksum = get_checksum('FASTATUS,0')
     assert cksum == '0F'
+
+
+def test_verify_checksum():
+    assert verify_checksum('$FASTATUS,0*0F\r\n')
+    assert not verify_checksum('$FASTATUS,0*0E\r\n')
